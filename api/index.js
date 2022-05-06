bdbb7fef3d76b0f1a17c0ec99c2b732a0f67ebf4
Mohammed3tef <mohammedatef2050@gmail.com>
2022-05-06 11:20:11 +0200

    update user account

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 
@@ -12,9 +13,9 @@ async function main() {
   await mongoose.connect(process.env.DATABASE);
 }
 
-
-app.use(express.json())
+app.use(express.json());
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8000, () => {
   console.log("Backend server is running !");
