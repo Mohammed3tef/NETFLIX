32bf8423c6f39dc67c48555751155c8ade37a610
Mohammed3tef <mohammedatef2050@gmail.com>
2022-05-06 08:54:23 +0200

    login with jwt authentication

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
-
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   const newUser = new User({
@@ -18,6 +18,30 @@ router.post("/register", async (req, res) => {
     res.status(200).json(user);
   } catch (e) {
     res.status(500).json(e);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("wrong email address");
+
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password &&
+      res.status(401).json("wrong password");
+
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: "5d" }
+    );
+    const { password, ...info } = user._doc;
+
+    res.status(200).json({ ...info, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
