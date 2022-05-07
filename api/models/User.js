9e7953ba6dd3bb23010afb735d60602b30899005
Mohammed3tef <mohammedatef2050@gmail.com>
2022-05-07 04:23:43 +0200

    get one user and all users and stats

--- api/models/User.js
+++ api/models/User.js
@@ -3,8 +3,19 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
     profilePic: { type: String, defaut: "" },
     isAdmin: { type: Boolean, default: false },
   },
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -16,8 +16,8 @@ router.post("/register", async (req, res) => {
   try {
     const user = await newUser.save();
     res.status(200).json(user);
-  } catch (e) {
-    res.status(500).json(e);
+  } catch (err) {
+    res.status(500).send(err);
   }
 });
 
@@ -41,7 +41,8 @@ router.post("/login", async (req, res) => {
 
     res.status(200).json({ ...info, accessToken });
   } catch (err) {
-    res.status(500).json(err);
+    console.log(err);
+    res.end();
   }
 });
 
