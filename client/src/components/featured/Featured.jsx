39b581babb4e47868e8f496d8e05c6da372b2517
Mohammed3tef <mohammedatef2050@gmail.com>
2022-05-11 21:09:58 +0200

    edit watch page urls and fetching featured movie

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./featured.scss";
 import { PlayArrow, InfoOutlined } from "@mui/icons-material";
+import axios from "axios";
 const Featured = ({type}) => {
+  const[content,setContenet] = useState({});
+
+  useEffect(()=>{
+    const getRandomContent = async()=>{
+      try {
+        const res = await axios.get(`/movies/random?type=${type}`, {
+          header: { token: "Bearer" },
+        });
+        setContenet(res.data[0]);
+      } catch (err) {
+        console.log(err)
+
+        
+      }
+    }
+    getRandomContent();
+  },[type])
   return (
     <div className="featured">
       {type && (
@@ -25,20 +43,11 @@ const Featured = ({type}) => {
           </select>
         </div>
       )}
-      <img
-        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-        alt=""
-      />
+      <img src={content.img} alt="" />
       <div className="info">
-        <img
-          src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1"
-          alt=""
-        />
+        <img src={content.imgTitle} alt="" />
         <span className="desc">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-          adipisci repellendus eum quasi illo, velit numquam, maxime tempora
-          sint deleniti, aliquid qui? Facilis, adipisci! Ratione hic repudiandae
-          temporibus eum earum?
+          {content.desc}
         </span>
         <div className="buttons">
           <button className="play">
--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -7,6 +7,7 @@ import {
   ThumbDownOutlined,
 } from "@mui/icons-material";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const ListItem = ({ index, item }) => {
   const [isHovered, setIsHovered] = useState(false);
@@ -17,43 +18,45 @@ const ListItem = ({ index, item }) => {
         const res = await axios.get("/movies/find/" + item, {
           header: { token: "Bearer" },
         });
-        setMovie(res.data)
+        setMovie(res.data);
       } catch (err) {
         console.log(err);
       }
     };
-    getMovie()
-  },[item]);
+    getMovie();
+  }, [item]);
 
   return (
-    <div
-      className="listItem"
-      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <img src={movie.img} alt="" />
-      {isHovered && (
-        <>
-          <video src={movie.trailer} autoPlay={true} loop />
-          <div className="itemInfo">
-            <div className="icons">
-              <PlayArrow className="icon" />
-              <Add className="icon" />
-              <ThumbUpAltOutlined className="icon" />
-              <ThumbDownOutlined className="icon" />
+    <Link to={{pathname:"/watch",movie:movie}}>
+      <div
+        className="listItem"
+        style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <img src={movie.img} alt="" />
+        {isHovered && (
+          <>
+            <video src={movie.trailer} autoPlay={true} loop />
+            <div className="itemInfo">
+              <div className="icons">
+                <PlayArrow className="icon" />
+                <Add className="icon" />
+                <ThumbUpAltOutlined className="icon" />
+                <ThumbDownOutlined className="icon" />
+              </div>
+              <div className="itemInfoTop">
+                <span>{movie.duration}</span>
+                <span className="limit">+{movie.limit}</span>
+                <span>{movie.year}</span>
+              </div>
+              <div className="desc">{movie.desc}</div>
+              <div className="genre">{movie.genre}</div>
             </div>
-            <div className="itemInfoTop">
-              <span>{movie.duration}</span>
-              <span className="limit">+{movie.limit}</span>
-              <span>{movie.year}</span>
-            </div>
-            <div className="desc">{movie.desc}</div>
-            <div className="genre">{movie.genre}</div>
-          </div>
-        </>
-      )}
-    </div>
+          </>
+        )}
+      </div>
+    </Link>
   );
 };
 
